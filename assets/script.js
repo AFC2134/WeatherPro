4a68767446a41f5a8fcdc7b8590aef55cc7d6855
afc2134 <85919290+AFC2134@users.noreply.github.com>
2021-09-21 16:32:43 -0500

    click handler, top vars, begin api call func

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+var cityNameEl = document.getElementById("city");
+var fiveDayEl = document.getElementById("fiveDayForecast");
+
+
+function getWeatherData() {
+    var apiCall =
+}
+
+
+
+
+document.getElementById("searchCity").addEventListener("click", getWeatherData);
